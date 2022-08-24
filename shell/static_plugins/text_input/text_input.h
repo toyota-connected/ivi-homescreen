1649f74c5675c1f0cfcc0925739edcdaa4294a12
Kazuyoshi Akiyama <kazuyoshi.akiyama@woven-planet.global>
2022-08-24 14:46:13 +0900

    Add Woven Alpha License Header
    I added Woven Alpha License Header to all files I will changed after
    this commit.
    
    Signed-off-by: Kazuyoshi Akiyama <kazuyoshi.akiyama@woven-planet.global>

--- shell/static_plugins/text_input/text_input.h
+++ shell/static_plugins/text_input/text_input.h
@@ -1,5 +1,6 @@
 /*
  * Copyright 2020-2022 Toyota Connected North America
+ * @copyright Copyright (c) 2022 Woven Alpha, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
