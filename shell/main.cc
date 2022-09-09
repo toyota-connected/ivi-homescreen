1239063ae55a1b5fc7562e2f376fe5a3892ae5ed
Joel Winarske <joel.winarske@gmail.com>
2022-09-09 13:31:43 -0700

    xdg_shell app-id CLI argument (#84)
    -add xdg-shell-app-id CLI argument that sets the app-id.
    
    Signed-off-by: Joel Winarske <joel.winarske@gmail.com>

--- shell/main.cc
+++ shell/main.cc
@@ -239,6 +239,17 @@ int main(int argc, char** argv) {
       config.view.pixel_ratio = strtod(pixel_ratio_str.c_str(), &ptr);
       RemoveArgument(config.view.vm_args, "--p=" + pixel_ratio_str);
     }
+    if (cl.HasOption("xdg-shell-app-id")) {
+      cl.GetOptionValue("xdg-shell-app-id", &config.app_id);
+      if (config.app_id.empty()) {
+        FML_LOG(ERROR) << "--xdg-shell-app-id option requires an argument "
+                          "(e.g. --xdg-shell-app-id=gallery)";
+        return EXIT_FAILURE;
+      }
+      FML_DLOG(INFO) << "Application ID: " << config.app_id;
+      RemoveArgument(config.view.vm_args,
+                     "--xdg-shell-app-id=" + config.app_id);
+    }
   }
 
   auto vm_arg_count = config.view.vm_args.size();
