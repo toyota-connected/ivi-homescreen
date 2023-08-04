eac80718428b0f60a8ef2656dfabf612ad301985
takaaki <takaaki_haga@mail.toyota.co.jp>
2023-08-04 10:21:05 +0900

    fix typo

--- test/unit_test/utils-test/test_case_utils.cc
+++ test/unit_test/utils-test/test_case_utils.cc
@@ -3,19 +3,19 @@
 #include "gtest/gtest.h"
 #include "utils.h"
 
-TEST(utils_rtrim, Normalcalse_01) {
+TEST(utils_rtrim, Normalcase_01) {
     std::string input = "unit test";
     std::string output = Utils::rtrim(input, "t");
     EXPECT_EQ("unit tes", output);
 }
 
-TEST(utils_ltrim, Normalcalse_01) {
+TEST(utils_ltrim, Normalcase_01) {
     std::string input = "unit test";
     std::string output = Utils::ltrim(input, "u");
     EXPECT_EQ("nit test", output);
 }
 
-TEST(utils_trim, Normalcalse_01) {
+TEST(utils_trim, Normalcase_01) {
     std::string input = "unit test";
     std::string output = Utils::trim(input, "ut");
     EXPECT_EQ("nit tes", output);
