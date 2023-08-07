4c8c4d20e0ce17e9542c874237ae4b351f39ccb5
takaaki <takaaki_haga@mail.toyota.co.jp>
2023-08-07 13:01:56 +0900

    fix test case name

--- test/unit_test/utils-test/test_case_utils.cc
+++ test/unit_test/utils-test/test_case_utils.cc
@@ -4,36 +4,36 @@
 #include "utils.h"
 
 /****************************************************************
-Test Case Name.Test Name： ivi-homescreen_utils_rtrim_Lv1Normal001
+Test Case Name.Test Name： homescreen_utils_rtrim_Lv1Normal001
 Use Case Name: Intialization
 Test Summary：Test the function of rtrim
 ***************************************************************/
 
-TEST(ivi-homescreen_utils_rtrim, Lv1Normal001) {
+TEST(homescreen_utils_rtrim, Lv1Normal001) {
     std::string input = "unit test";
     std::string output = Utils::rtrim(input, "t");
     EXPECT_EQ("unit tes", output);
 }
 
 /****************************************************************
-Test Case Name.Test Name： ivi-homescreen_utils_ltrim_Lv1Normal001
+Test Case Name.Test Name： homescreen_utils_ltrim_Lv1Normal001
 Use Case Name: Intialization
 Test Summary：Test the function of ltrim
 ***************************************************************/
 
-TEST(ivi-homescreen_utils_ltrim, Lv1Normal001) {
+TEST(homescreen_utils_ltrim, Lv1Normal001) {
     std::string input = "unit test";
     std::string output = Utils::ltrim(input, "u");
     EXPECT_EQ("nit test", output);
 }
 
 /****************************************************************
-Test Case Name.Test Name： ivi-homescreen_utils_trim_Lv1Normal001
+Test Case Name.Test Name： homescreen_utils_trim_Lv1Normal001
 Use Case Name: Intialization
 Test Summary：Test the function of trim
 ***************************************************************/
 
-TEST(ivi-homescreen_utils_trim, Lv1Normal001) {
+TEST(homescreen_utils_trim, Lv1Normal001) {
     std::string input = "unit test";
     std::string output = Utils::trim(input, "ut");
     EXPECT_EQ("nit tes", output);
