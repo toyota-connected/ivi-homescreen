e3d7a74bd02978aea5c1cc816eb7088f7de644cc
takaaki <takaaki.haga@woven-planet.global>
2023-10-12 14:54:03 +0900

    Refactoring configuration - Simplified PrintConfig to not use sstream - Added UT for PrintConfig by visual confirmation - Remove the process to set debug_backend parameter from getViewParameters - Create a function to set JSON parameters in config member

--- shell/configuration/configuration.cc
+++ shell/configuration/configuration.cc
@@ -16,7 +16,6 @@
 #include "configuration.h"
 
 #include <fstream>
-#include <sstream>
 #include <filesystem>
 
 #include <rapidjson/document.h>
@@ -47,25 +46,23 @@ void Configuration::getViewParameters(
   if (obj.HasMember(kWindowTypeKey) && obj[kWindowTypeKey].IsString()) {
     instance.view.window_type = obj[kWindowTypeKey].GetString();
   }
-  if (obj.HasMember(kOutputIndex) && obj[kOutputIndex].IsInt()) {
+  if (obj.HasMember(kOutputIndex) && obj[kOutputIndex].IsUint()) {
     instance.view.wl_output_index =
-        static_cast<uint32_t>(obj[kOutputIndex].GetInt());
+        static_cast<uint32_t>(obj[kOutputIndex].GetUint());
   }
-  if (obj.HasMember(kWidthKey) && obj[kWidthKey].IsInt()) {
-    instance.view.width = static_cast<uint32_t>(obj[kWidthKey].GetInt());
+  if (obj.HasMember(kWidthKey) && obj[kWidthKey].IsUint()) {
+    instance.view.width = obj[kWidthKey].GetUint();
   }
-  if (obj.HasMember(kHeightKey) && obj[kHeightKey].IsInt()) {
-    instance.view.height = static_cast<uint32_t>(obj[kHeightKey].GetInt());
+  if (obj.HasMember(kHeightKey) && obj[kHeightKey].IsUint()) {
+    instance.view.height = obj[kHeightKey].GetUint();
   }
-  if (obj.HasMember(kPixelRatioKey) && obj[kPixelRatioKey].IsDouble()) {
-    instance.view.pixel_ratio = obj[kPixelRatioKey].GetDouble();
+  if (obj.HasMember(kPixelRatioKey)) {
+    if (obj[kPixelRatioKey].IsDouble() || obj[kPixelRatioKey].IsInt()) {
+      instance.view.pixel_ratio = obj[kPixelRatioKey].GetDouble();
+    }
   }
-  if (obj.HasMember(kPixelRatioKey) && obj[kPixelRatioKey].IsInt()) {
-    instance.view.pixel_ratio = static_cast<double>(obj[kPixelRatioKey].GetInt());
-  }
-  if (obj.HasMember(kIviSurfaceIdKey) && obj[kIviSurfaceIdKey].IsInt()) {
-    instance.view.ivi_surface_id =
-        static_cast<uint32_t>(obj[kIviSurfaceIdKey].GetInt());
+  if (obj.HasMember(kIviSurfaceIdKey) && obj[kIviSurfaceIdKey].IsUint()) {
+    instance.view.ivi_surface_id = obj[kIviSurfaceIdKey].GetUint();
   }
   if (obj.HasMember(kAccessibilityFeaturesKey) &&
       obj[kAccessibilityFeaturesKey].IsInt()) {
@@ -84,17 +81,14 @@ void Configuration::getViewParameters(
   if (obj.HasMember(kDebugBackendKey) && obj[kDebugBackendKey].IsBool()) {
     instance.debug_backend = obj[kDebugBackendKey].GetBool();
   }
-  if (obj.HasMember(kFpsOutputConsole) && obj[kFpsOutputConsole].IsInt()) {
-    instance.view.fps_output_console =
-        static_cast<uint32_t>(obj[kFpsOutputConsole].GetInt());
+  if (obj.HasMember(kFpsOutputConsole) && obj[kFpsOutputConsole].IsUint()) {
+    instance.view.fps_output_console = obj[kFpsOutputConsole].GetUint();
   }
-  if (obj.HasMember(kFpsOutputOverlay) && obj[kFpsOutputOverlay].IsInt()) {
-    instance.view.fps_output_overlay =
-        static_cast<uint32_t>(obj[kFpsOutputOverlay].GetInt());
+  if (obj.HasMember(kFpsOutputOverlay) && obj[kFpsOutputOverlay].IsUint()) {
+    instance.view.fps_output_overlay = obj[kFpsOutputOverlay].GetUint();
   }
-  if (obj.HasMember(kFpsOutputFrequency) && obj[kFpsOutputFrequency].IsInt()) {
-    instance.view.fps_output_frequency =
-        static_cast<uint32_t>(obj[kFpsOutputFrequency].GetInt());
+  if (obj.HasMember(kFpsOutputFrequency) && obj[kFpsOutputFrequency].IsUint()) {
+    instance.view.fps_output_frequency = obj[kFpsOutputFrequency].GetUint();
   }
 }
 
@@ -117,57 +111,8 @@ void Configuration::getGlobalParameters(
   if (obj.HasMember(kDebugBackendKey) && obj[kDebugBackendKey].IsBool()) {
     instance.debug_backend = obj[kDebugBackendKey].GetBool();
   }
-  if (obj.HasMember(kVmArgsKey) && obj[kVmArgsKey].IsArray()) {
-    auto args = obj[kVmArgsKey].GetArray();
-    for (auto const& arg : args) {
-      instance.view.vm_args.emplace_back(arg.GetString());
-    }
-  }
-  if (obj.HasMember(kBundlePathKey) && obj[kBundlePathKey].IsString()) {
-    instance.view.bundle_path = obj[kBundlePathKey].GetString();
-  }
-  if (obj.HasMember(kWindowTypeKey) && obj[kWindowTypeKey].IsString()) {
-    instance.view.window_type = obj[kWindowTypeKey].GetString();
-  }
-  if (obj.HasMember(kOutputIndex) && obj[kOutputIndex].IsInt()) {
-    instance.view.wl_output_index = obj[kOutputIndex].GetUint();
-  }
-  if (obj.HasMember(kAccessibilityFeaturesKey) &&
-      obj[kAccessibilityFeaturesKey].IsInt()) {
-    instance.view.accessibility_features =
-        MaskAccessibilityFeatures(obj[kAccessibilityFeaturesKey].GetInt());
-  }
-  if (obj.HasMember(kWidthKey) && obj[kWidthKey].IsInt()) {
-    instance.view.width = static_cast<uint32_t>(obj[kWidthKey].GetInt());
-  }
-  if (obj.HasMember(kHeightKey) && obj[kHeightKey].IsInt()) {
-    instance.view.height = static_cast<uint32_t>(obj[kHeightKey].GetInt());
-  }
-  if (obj.HasMember(kPixelRatioKey) && obj[kPixelRatioKey].IsDouble()) {
-    instance.view.pixel_ratio = obj[kPixelRatioKey].GetDouble();
-  }
-  if (obj.HasMember(kPixelRatioKey) && obj[kPixelRatioKey].IsInt()) {
-    instance.view.pixel_ratio = static_cast<double>(obj[kPixelRatioKey].GetInt());
-  }
-  if (obj.HasMember(kIviSurfaceIdKey) && obj[kIviSurfaceIdKey].IsInt()) {
-    instance.view.ivi_surface_id =
-        static_cast<uint32_t>(obj[kIviSurfaceIdKey].GetInt());
-  }
-  if (obj.HasMember(kFullscreenKey) && obj[kFullscreenKey].IsBool()) {
-    instance.view.fullscreen = obj[kFullscreenKey].GetBool();
-  }
-  if (obj.HasMember(kFpsOutputConsole) && obj[kFpsOutputConsole].IsInt()) {
-    instance.view.fps_output_console =
-        static_cast<uint32_t>(obj[kFpsOutputConsole].GetInt());
-  }
-  if (obj.HasMember(kFpsOutputOverlay) && obj[kFpsOutputOverlay].IsInt()) {
-    instance.view.fps_output_overlay =
-        static_cast<uint32_t>(obj[kFpsOutputOverlay].GetInt());
-  }
-  if (obj.HasMember(kFpsOutputFrequency) && obj[kFpsOutputFrequency].IsInt()) {
-    instance.view.fps_output_frequency =
-        static_cast<uint32_t>(obj[kFpsOutputFrequency].GetInt());
-  }
+
+  Configuration::getViewParameters(obj, instance);
 }
 
 void Configuration::getView(rapidjson::Document& doc,
@@ -310,47 +255,25 @@ rapidjson::Document Configuration::getJsonDocument(
 }
 
 void Configuration::PrintConfig(const Config& config) {
-  std::stringstream ss;
-  ss << kGitBranch << " @ " << kGitCommitHash;
-  spdlog::info(ss.str().c_str());
-  ss.str("");
-  ss.clear();
-
+  spdlog::info("{} @ {}", kGitBranch, kGitCommitHash);
   spdlog::info("**********");
   spdlog::info("* Global *");
   spdlog::info("**********");
   if (!config.app_id.empty()) {
-    ss << "Application Id: .......... " << config.app_id;
-    spdlog::info(ss.str().c_str());
-    ss.str("");
-    ss.clear();
+    spdlog::info("Application Id: .......... {}", config.app_id);
   }
   if (!config.json_configuration_path.empty()) {
-    ss << "JSON Configuration: ...... " << config.json_configuration_path;
-    spdlog::info(ss.str().c_str());
-    ss.str("");
-    ss.clear();
+    spdlog::info("JSON Configuration: ...... {}", config.json_configuration_path);
   }
   if (!config.cursor_theme.empty()) {
-    ss << "Cursor Theme: ............ " << config.cursor_theme;
-    spdlog::info(ss.str().c_str());
-    ss.str("");
-    ss.clear();
+    spdlog::info("Cursor Theme: ............ {}", config.cursor_theme);
   }
-  ss << "Disable Cursor: .......... " << (config.disable_cursor ? "true" : "false");
-  spdlog::info(ss.str().c_str());
-  ss.str("");
-  ss.clear();
+  spdlog::info("Disable Cursor: .......... {}", config.disable_cursor ? "true" : "false");
+
   if (!config.wayland_event_mask.empty()) {
-    ss << "Wayland Event Mask: ...... " << config.wayland_event_mask;
-    spdlog::info(ss.str().c_str());
-    ss.str("");
-    ss.clear();
+    spdlog::info("Wayland Event Mask: ...... {}", config.wayland_event_mask);
   }
-  ss << "Debug Backend: ........... " << (config.debug_backend ? "true" : "false");
-  spdlog::info(ss.str().c_str());
-  ss.str("");
-  ss.clear();
+  spdlog::info("Debug Backend: ........... {}", config.debug_backend ? "true" : "false");
   spdlog::info("********");
   spdlog::info("* View *");
   spdlog::info("********");
@@ -360,44 +283,18 @@ void Configuration::PrintConfig(const Config& config) {
       spdlog::info(arg);
     }
   }
-  ss << "Bundle Path: .............. " << config.view.bundle_path;
-  spdlog::info(ss.str().c_str());
-  ss.str("");
-  ss.clear();
-  ss << "Window Type: .............. " << config.view.window_type;
-  spdlog::info(ss.str().c_str());
-  ss.str("");
-  ss.clear();
-  ss << "Output Index: ............. " << config.view.wl_output_index;
-  spdlog::info(ss.str().c_str());
-  ss.str("");
-  ss.clear();
-  ss << "Size: ..................... " << config.view.width << " x "
-     << config.view.height;
-  spdlog::info(ss.str().c_str());
-  ss.str("");
-  ss.clear();
+  spdlog::info("Bundle Path: .............. {}", config.view.bundle_path);
+  spdlog::info("Window Type: .............. {}", config.view.window_type);
+  spdlog::info("Output Index: ............. {}", config.view.wl_output_index);
+  spdlog::info("Size: ..................... {} x {}", config.view.width, config.view.height);
   if (config.view.pixel_ratio != kDefaultPixelRatio) {
-    ss << "Pixel Ratio: .............. " << config.view.pixel_ratio;
-    spdlog::info(ss.str().c_str());
-    ss.str("");
-    ss.clear();
+    spdlog::info("Pixel Ratio: .............. {}", config.view.pixel_ratio);
   }
   if (config.view.ivi_surface_id > 0) {
-    ss << "IVI Surface ID: ........... " << config.view.ivi_surface_id;
-    spdlog::info(ss.str().c_str());
-    ss.str("");
-    ss.clear();
+    spdlog::info("IVI Surface ID: ........... {}", config.view.ivi_surface_id);
   }
-  ss << "Fullscreen: ............... "
-     << (config.view.fullscreen ? "true" : "false");
-  spdlog::info(ss.str().c_str());
-  ss.str("");
-  ss.clear();
-  ss << "Accessibility Features: ... " << config.view.accessibility_features;
-  spdlog::info(ss.str().c_str());
-  ss.str("");
-  ss.clear();
+  spdlog::info("Fullscreen: ............... {}", config.view.fullscreen ? "true" : "false");
+  spdlog::info("Accessibility Features: ... {}", config.view.accessibility_features);
 }
 
 Configuration::Config Configuration::ConfigFromArgcArgv(int argc, const char* const* argv) {
---
+++ test/unit_test/configuration-test-case/test_case_configuration_PrintConfig.cc
@@ -0,0 +1,66 @@
+#include <filesystem>
+#include <stdexcept>
+#include <gtest/gtest.h>
+#include <iostream>
+
+#include <configuration/configuration.h>
+
+/****************************************************************
+Test Case Name.Test Name： HomescreenConfigurationPrintConfig_Lv1Normal001
+Use Case Name: Initialization
+Test Summary：Test PrintConfig to visually check
+***************************************************************/
+
+TEST(HomescreenConfigurationPrintConfig, Lv1Normal001) {
+  // set test parameters
+  struct Configuration::Config config {};
+  config.app_id = "homescreen";
+  config.json_configuration_path = "/usr/share/";
+  config.cursor_theme = "DMZ-White";
+  config.disable_cursor = true;
+  config.disable_cursor_set = true;
+  config.wayland_event_mask = "keyboard";
+  config.debug_backend = true;
+  config.debug_backend_set = true;
+  config.view.vm_args.push_back("--enable-asserts");
+  config.view.vm_args.push_back("--pause-isolates-on-start");
+  config.view.bundle_path = "/home/";
+  config.view.window_type = "NORMAL";
+  config.view.wl_output_index = 1;
+  config.view.accessibility_features = 1;
+  config.view.width = 1280;
+  config.view.height = 720;
+  config.view.pixel_ratio = 1.234;
+  config.view.ivi_surface_id = 1;
+  config.view.fullscreen = true;
+  config.view.fullscreen_set = true;
+
+  std::cout << "\n##################### Reference Output #######################\n" << std::endl;
+  std::cout << "[20xx-xx-xx xx:xx:xx.xxx] [info] {branch} @ {commit id}" << std::endl;
+  std::cout << "[20xx-xx-xx xx:xx:xx.xxx] [info] **********" << std::endl;
+  std::cout << "[20xx-xx-xx xx:xx:xx.xxx] [info] * Global *" << std::endl;
+  std::cout << "[20xx-xx-xx xx:xx:xx.xxx] [info] **********" << std::endl;
+  std::cout << "[20xx-xx-xx xx:xx:xx.xxx] [info] Application Id: .......... homescreen" << std::endl;
+  std::cout << "[20xx-xx-xx xx:xx:xx.xxx] [info] JSON Configuration: ...... /usr/share/" << std::endl;
+  std::cout << "[20xx-xx-xx xx:xx:xx.xxx] [info] Cursor Theme: ............ DMZ-White" << std::endl;
+  std::cout << "[20xx-xx-xx xx:xx:xx.xxx] [info] Disable Cursor: .......... true" << std::endl;
+  std::cout << "[20xx-xx-xx xx:xx:xx.xxx] [info] Wayland Event Mask: ...... keyboard" << std::endl;
+  std::cout << "[20xx-xx-xx xx:xx:xx.xxx] [info] Debug Backend: ........... true" << std::endl;
+  std::cout << "[20xx-xx-xx xx:xx:xx.xxx] [info] ********" << std::endl;
+  std::cout << "[20xx-xx-xx xx:xx:xx.xxx] [info] * View *" << std::endl;
+  std::cout << "[20xx-xx-xx xx:xx:xx.xxx] [info] ********" << std::endl;
+  std::cout << "[20xx-xx-xx xx:xx:xx.xxx] [info] VM Args:" << std::endl;
+  std::cout << "[20xx-xx-xx xx:xx:xx.xxx] [info] --enable-asserts" << std::endl;
+  std::cout << "[20xx-xx-xx xx:xx:xx.xxx] [info] --pause-isolates-on-start" << std::endl;
+  std::cout << "[20xx-xx-xx xx:xx:xx.xxx] [info] Bundle Path: .............. /home/" << std::endl;
+  std::cout << "[20xx-xx-xx xx:xx:xx.xxx] [info] Window Type: .............. NORMAL" << std::endl;
+  std::cout << "[20xx-xx-xx xx:xx:xx.xxx] [info] Output Index: ............. 1" << std::endl;
+  std::cout << "[20xx-xx-xx xx:xx:xx.xxx] [info] Size: ..................... 1280 x 720" << std::endl;
+  std::cout << "[20xx-xx-xx xx:xx:xx.xxx] [info] Pixel Ratio: .............. 1.234" << std::endl;
+  std::cout << "[20xx-xx-xx xx:xx:xx.xxx] [info] Ivi Surface ID: ........... 1" << std::endl;
+  std::cout << "[20xx-xx-xx xx:xx:xx.xxx] [info] Fullscreen: ............... true" << std::endl;
+  std::cout << "[20xx-xx-xx xx:xx:xx.xxx] [info] Accessibility Features: ... 1" << std::endl;
+  std::cout << "\n################## Please check visually #####################\n" << std::endl;
+
+  Configuration::PrintConfig(config);
+}
