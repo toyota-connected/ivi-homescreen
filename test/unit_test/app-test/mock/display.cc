e060c2d98fc585ab29971d9705aafa502cf36d6d
Joel Winarske <joel.winarske@woven-planet.global>
2023-10-16 10:05:37 -0700

    UT missing method
    -implement missing method in UT app-test
    -convert FML_LOG usage from AGL cherry pick
    
    Signed-off-by: Joel Winarske <joel.winarske@woven-planet.global>

--- test/unit_test/app-test/mock/display.cc
+++ test/unit_test/app-test/mock/display.cc
@@ -699,7 +699,7 @@ const struct wl_touch_listener Display::touch_listener = {
 };
 
 int Display::PollEvents() {
-   /* Delete implementation */
+  /* Delete implementation */
   return 1;
 }
 
@@ -723,6 +723,27 @@ void Display::AglShellDoReady() const {
   if (m_agl.shell) {
     agl_shell_ready(m_agl.shell);
   }
+}
+
+void Display::AglShellDoSetupActivationArea(uint32_t x,
+                                            uint32_t y,
+                                            uint32_t index) {
+  uint32_t width = m_all_outputs[index]->width;
+  uint32_t height = m_all_outputs[index]->height - (2 * y);
+
+  if (!m_agl.shell)
+    return;
+
+  if (m_all_outputs[index]->transform == WL_OUTPUT_TRANSFORM_90) {
+    width = m_all_outputs[index]->height;
+    height = m_all_outputs[index]->width - (2 * y);
+  }
+
+  SPDLOG_DEBUG("Using custom rectangle [{}x{}+{}x{}] for activation", width,
+               height, x, y);
+
+  agl_shell_set_activate_region(m_agl.shell, m_all_outputs[index]->output, x, y,
+                                width, height);
 }
 
 void Display::SetEngine(wl_surface* surface, Engine* engine) {
