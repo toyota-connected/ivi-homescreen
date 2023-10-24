46b45142ee620a3b9ff5353f7244ef8fd2414b88
takaaki <takaaki_haga@mail.toyota.co.jp>
2023-10-24 18:32:47 +0900

    add ut for texture

---
+++ test/unit_test/mock/engine.cc
@@ -0,0 +1,523 @@
+// Copyright 2020 Toyota Connected North America
+// @copyright Copyright (c) 2022 Woven Alpha, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <utility>
+#include <vector>
+
+#include <dlfcn.h>
+#include <cassert>
+
+#include <flutter/fml/file.h>
+#include <flutter/fml/paths.h>
+
+#include "constants.h"
+#include "engine.h"
+#include "platform_channel.h"
+#include "textures/texture.h"
+
+#include "hexdump.h"
+#include "utils.h"
+
+Engine::Engine(FlutterView* view,
+               const size_t index,
+               const std::vector<const char*>& vm_args_c,
+               const std::string& bundle_path,
+               const int32_t accessibility_features)
+    : m_index(index),
+      m_running(false),
+      m_backend(view->GetBackend()),
+      m_egl_window(view->GetWindow()),
+      m_view(view),
+      m_cache_path(std::move(GetFilePath(index))),
+      m_prev_height(0),
+      m_prev_width(0),
+      m_prev_pixel_ratio(1.0),
+      m_accessibility_features(accessibility_features),
+      m_flutter_engine(nullptr),
+      m_args({}) {
+  /* Delete implementation */
+}
+
+Engine::~Engine() {
+  if (m_running) {
+    LibFlutterEngine->Deinitialize(m_flutter_engine);
+    LibFlutterEngine->Shutdown(m_flutter_engine);
+    if (m_aot_data) {
+      LibFlutterEngine->CollectAOTData(m_aot_data);
+    }
+  }
+  m_platform_task_runner.reset();
+  m_vm_queue.reset();
+}
+
+FlutterEngineResult Engine::RunTask() {
+  if (!m_flutter_engine) {
+    return kSuccess;
+  }
+
+  if (!m_vm_queue->empty()) {
+    std::scoped_lock<std::mutex> lock(m_queue_lock);
+    for (int i = 0; i < kVmLogChunkMax; i++) {
+      spdlog::info(m_vm_queue->front());
+      m_vm_queue->pop();
+      if (m_vm_queue->empty())
+        break;
+    }
+  }
+
+  return kSuccess;
+}
+
+FlutterEngineResult Engine::Shutdown() const {
+  if (!m_flutter_engine) {
+    return kSuccess;
+  }
+  return LibFlutterEngine->Shutdown(m_flutter_engine);
+}
+
+bool Engine::IsRunning() const {
+  return m_running;
+}
+
+FlutterEngineResult Engine::Run(pthread_t event_loop_thread_id) {
+  SPDLOG_TRACE("({}) +Engine::Run", m_index);
+
+  const auto config = m_backend->GetRenderConfig();
+  FlutterEngineResult result = LibFlutterEngine->Initialize(
+      FLUTTER_ENGINE_VERSION, &config, &m_args, this, &m_flutter_engine);
+  if (result != kSuccess) {
+    spdlog::error("({}) FlutterEngineRun failed or engine is null", m_index);
+    return result;
+  }
+
+  m_event_loop_thread = event_loop_thread_id;
+
+  result = LibFlutterEngine->RunInitialized(m_flutter_engine);
+  if (result == kSuccess) {
+    m_running = true;
+    SPDLOG_DEBUG("({}) Engine::m_running = {}", m_index, m_running);
+  }
+
+  // Set available system locales
+  SetUpLocales();
+
+  // Set Accessibility Features
+  LibFlutterEngine->UpdateAccessibilityFeatures(
+      m_flutter_engine,
+      static_cast<FlutterAccessibilityFeature>(m_accessibility_features));
+
+  SPDLOG_TRACE("({}) -Engine::Run", m_index);
+  return result;
+}
+
+FlutterEngineResult Engine::SetWindowSize(size_t height, size_t width) {
+  if (!m_running) {
+    return kInternalInconsistency;
+  }
+
+  m_prev_height = height;
+  m_prev_width = width;
+
+  // Set window size
+  const FlutterWindowMetricsEvent fwme = {
+      .struct_size = sizeof(FlutterWindowMetricsEvent),
+      .width = width,
+      .height = height,
+      .pixel_ratio = m_prev_pixel_ratio};
+
+  const auto result =
+      LibFlutterEngine->SendWindowMetricsEvent(m_flutter_engine, &fwme);
+  if (result != kSuccess) {
+    spdlog::critical("({}) Failed send initial window size to flutter",
+                     m_index);
+    assert(false);
+  }
+
+  return kSuccess;
+}
+
+FlutterEngineResult Engine::SetPixelRatio(double pixel_ratio) {
+  if (!m_running) {
+    return kInternalInconsistency;
+  }
+
+  assert(m_prev_width);
+  assert(m_prev_height);
+
+  m_prev_pixel_ratio = pixel_ratio;
+
+  // Set window size
+  const FlutterWindowMetricsEvent fwme = {
+      .struct_size = sizeof(FlutterWindowMetricsEvent),
+      .width = m_prev_width,
+      .height = m_prev_height,
+      .pixel_ratio = pixel_ratio};
+
+  const auto result =
+      LibFlutterEngine->SendWindowMetricsEvent(m_flutter_engine, &fwme);
+  if (result != kSuccess) {
+    spdlog::critical("({}) Failed send initial window size to flutter",
+                     m_index);
+    assert(false);
+  }
+
+  SPDLOG_DEBUG("({}) SetWindowSize: width={}, height={}, pixel_ratio={}",
+               m_index, m_prev_width, m_prev_height, pixel_ratio);
+  return kSuccess;
+}
+
+FlutterEngineResult Engine::TextureRegistryAdd(int64_t texture_id,
+                                               Texture* texture) {
+  this->m_texture_registry[texture_id] = texture;
+  SPDLOG_DEBUG("({}) Added Texture ({}) to registry", m_index, texture_id);
+  return kSuccess;
+}
+
+MAYBE_UNUSED FlutterEngineResult
+Engine::TextureRegistryRemove(int64_t texture_id) {
+  const auto search =
+      std::find_if(m_texture_registry.begin(), m_texture_registry.end(),
+                   [&texture_id](const std::pair<int64_t, void*>& element) {
+                     return element.first == texture_id;
+                   });
+  if (search != m_texture_registry.end()) {
+    SPDLOG_DEBUG("({}) Removing Texture ({}) from registry {}", m_index,
+                 texture_id, static_cast<void*>(search->second));
+    m_texture_registry.erase(search);
+    spdlog::info("({}) Removed Texture ({}) from registry", m_index,
+                 texture_id);
+
+    return kSuccess;
+  }
+  SPDLOG_DEBUG("({}) Texture Already removed from registry: ({})", m_index,
+               texture_id);
+  return kInvalidArguments;
+}
+
+FlutterEngineResult Engine::TextureEnable(int64_t texture_id) {
+  /* Delete implementation */
+  return kSuccess;
+}
+
+FlutterEngineResult Engine::TextureDisable(int64_t texture_id) {
+  /* Delete implementation */
+  return kSuccess;
+}
+
+FlutterEngineResult Engine::MarkExternalTextureFrameAvailable(
+    const Engine* engine,
+    const int64_t texture_id) {
+  /* Delete implementation */
+  return kSuccess;
+}
+
+flutter::EncodableValue Engine::TextureCreate(
+    int64_t texture_id,
+    int32_t width,
+    int32_t height,
+    const std::map<flutter::EncodableValue, flutter::EncodableValue>* args) {
+  SPDLOG_DEBUG("({}) Engine::TextureCreate: <{}>", m_index, texture_id);
+
+  const auto texture = this->m_texture_registry[texture_id];
+
+  if (texture != nullptr) {
+    return texture->Create(width, height, args);
+  }
+
+  return flutter::EncodableValue(flutter::EncodableMap{
+      {flutter::EncodableValue("result"), flutter::EncodableValue(-1)},
+      {flutter::EncodableValue("error"),
+       flutter::EncodableValue("Not found in registry")}});
+}
+
+std::string Engine::GetFilePath(size_t index) {
+  auto path = Utils::GetConfigHomePath();
+
+  if (!std::filesystem::is_directory(path) || !std::filesystem::exists(path)) {
+    if (!std::filesystem::create_directories(path)) {
+      spdlog::critical("({}) create_directories failed: {}", index, path);
+      exit(EXIT_FAILURE);
+    }
+  }
+
+  SPDLOG_DEBUG("({}) PersistentCachePath: {}", index, path);
+
+  return path;
+}
+
+FlutterEngineResult Engine::TextureDispose(int64_t texture_id) {
+  SPDLOG_DEBUG("({}) OpenGL Texture: dispose ({})", m_index, texture_id);
+
+  const auto search =
+      std::find_if(m_texture_registry.begin(), m_texture_registry.end(),
+                   [&texture_id](const std::pair<int64_t, void*>& element) {
+                     return element.first == texture_id;
+                   });
+  if (search != m_texture_registry.end()) {
+    ((Texture*)search->second)->Dispose(static_cast<uint32_t>(texture_id));
+    SPDLOG_DEBUG("({}) Texture Disposed ({})", m_index, texture_id);
+    return kSuccess;
+  }
+  return kInvalidArguments;
+}
+
+FlutterEngineResult Engine::SendPlatformMessageResponse(
+    const FlutterPlatformMessageResponseHandle* handle,
+    const uint8_t* data,
+    size_t data_length) const {
+  if (!m_running) {
+    return kInternalInconsistency;
+  }
+
+  if (!m_platform_task_runner->IsThreadEqual(pthread_self())) {
+    spdlog::error("Not sending message on Platform Thread");
+  }
+
+  return LibFlutterEngine->SendPlatformMessageResponse(m_flutter_engine, handle,
+                                                       data, data_length);
+}
+
+bool Engine::SendPlatformMessage(
+    const char* channel,
+    std::unique_ptr<std::vector<uint8_t>> message,
+    const FlutterPlatformMessageResponseHandle* response_handle) const {
+  if (!m_running) {
+    return false;
+  }
+
+  FlutterEngineResult result;
+  if (!m_platform_task_runner->IsThreadEqual(pthread_self())) {
+    auto f = m_platform_task_runner->QueuePlatformMessage(channel,
+                                                          std::move(message));
+    f.wait();
+    result = f.get();
+  } else {
+    const FlutterPlatformMessage msg{sizeof(FlutterPlatformMessage), channel,
+                                     message->data(), message->size(),
+                                     response_handle};
+    result = LibFlutterEngine->SendPlatformMessage(m_flutter_engine, &msg);
+  }
+
+  return (result == kSuccess);
+}
+
+bool Engine::SendPlatformMessage(const char* channel,
+                                 const uint8_t* message,
+                                 const size_t message_size) const {
+  if (!m_running) {
+    return false;
+  }
+
+  FlutterEngineResult result;
+  if (!m_platform_task_runner->IsThreadEqual(pthread_self())) {
+    auto msg =
+        std::make_unique<std::vector<uint8_t>>(message, message + message_size);
+    auto f =
+        m_platform_task_runner->QueuePlatformMessage(channel, std::move(msg));
+    f.wait();
+    result = f.get();
+  } else {
+    const FlutterPlatformMessage msg{sizeof(FlutterPlatformMessage), channel,
+                                     message, message_size, nullptr};
+    result = LibFlutterEngine->SendPlatformMessage(m_flutter_engine, &msg);
+  }
+  return (result == kSuccess);
+}
+
+bool Engine::SendPlatformMessage(const char* channel,
+                                 const uint8_t* message,
+                                 const size_t message_size,
+                                 const FlutterDataCallback reply,
+                                 void* userdata) const {
+  if (!m_running) {
+    return kInternalInconsistency;
+  }
+  FlutterPlatformMessageResponseHandle* handle;
+  LibFlutterEngine->PlatformMessageCreateResponseHandle(m_flutter_engine, reply,
+                                                        userdata, &handle);
+
+  FlutterEngineResult result;
+  if (!m_platform_task_runner->IsThreadEqual(pthread_self())) {
+    auto msg =
+        std::make_unique<std::vector<uint8_t>>(message, message + message_size);
+    auto f = m_platform_task_runner->QueuePlatformMessage(
+        channel, std::move(msg), handle);
+    f.wait();
+    result = f.get();
+  } else {
+    const FlutterPlatformMessage msg{
+        sizeof(FlutterPlatformMessage), channel, message, message_size, handle,
+    };
+
+    result = LibFlutterEngine->SendPlatformMessage(m_flutter_engine, &msg);
+  }
+  if (handle != nullptr) {
+    LibFlutterEngine->PlatformMessageReleaseResponseHandle(m_flutter_engine,
+                                                           handle);
+  }
+
+  return result == kSuccess;
+}
+
+MAYBE_UNUSED FlutterEngineResult
+Engine::UpdateAccessibilityFeatures(int32_t value) {
+  m_accessibility_features = value;
+  return LibFlutterEngine->UpdateAccessibilityFeatures(
+      m_flutter_engine, static_cast<FlutterAccessibilityFeature>(value));
+}
+
+// Passes locale information to the Flutter engine.
+void Engine::SetUpLocales() const {
+  constexpr FlutterLocale locale = {.struct_size = sizeof(FlutterLocale),
+                                    .language_code = kDefaultLocaleLanguageCode,
+                                    .country_code = kDefaultLocaleCountryCode,
+                                    .script_code = kDefaultLocaleScriptCode,
+                                    .variant_code = nullptr};
+
+  std::vector<const FlutterLocale*> flutter_locale_list;
+  flutter_locale_list.push_back(&locale);
+
+  FlutterEngineResult result;
+  if (!m_platform_task_runner->IsThreadEqual(pthread_self())) {
+    auto f = m_platform_task_runner->QueueUpdateLocales(
+        std::move(flutter_locale_list));
+    f.wait();
+    result = f.get();
+  } else {
+    result = LibFlutterEngine->UpdateLocales(m_flutter_engine,
+                                             flutter_locale_list.data(),
+                                             flutter_locale_list.size());
+  }
+
+  if (result != kSuccess) {
+    spdlog::error("({}) Failed to set up Flutter locales.", m_index);
+  }
+}
+
+void Engine::CoalesceMouseEvent(FlutterPointerSignalKind signal,
+                                FlutterPointerPhase phase,
+                                double x,
+                                double y,
+                                double scroll_delta_x,
+                                double scroll_delta_y,
+                                int64_t buttons) {
+  auto timestamp = LibFlutterEngine->GetCurrentTime() / 1000;
+  std::scoped_lock lock(m_pointer_mutex);
+  m_pointer_events.emplace_back(
+      FlutterPointerEvent{.struct_size = sizeof(FlutterPointerEvent),
+                          .phase = phase,
+#if defined(ENV64BIT)
+                          .timestamp = timestamp,
+#elif defined(ENV32BIT)
+                          .timestamp =
+                              static_cast<size_t>(timestamp & 0xFFFFFFFFULL),
+#endif
+                          .x = x,
+                          .y = y,
+                          .device = 0,
+                          .signal_kind = signal,
+                          .scroll_delta_x = scroll_delta_x,
+                          .scroll_delta_y = scroll_delta_y,
+                          .device_kind = kFlutterPointerDeviceKindMouse,
+                          .buttons = buttons,
+                          .pan_x = 0,
+                          .pan_y = 0,
+                          .scale = 0,
+                          .rotation = 0});
+}
+
+void Engine::CoalesceTouchEvent(FlutterPointerPhase phase,
+                                double x,
+                                double y,
+                                int32_t device) {
+  auto timestamp = LibFlutterEngine->GetCurrentTime() / 1000;
+  std::scoped_lock lock(m_pointer_mutex);
+  m_pointer_events.emplace_back(
+      FlutterPointerEvent{.struct_size = sizeof(FlutterPointerEvent),
+                          .phase = phase,
+#if defined(ENV64BIT)
+                          .timestamp = timestamp,
+#elif defined(ENV32BIT)
+                          .timestamp =
+                              static_cast<size_t>(timestamp & 0xFFFFFFFFULL),
+#endif
+                          .x = x,
+                          .y = y,
+                          .device = device,
+                          .signal_kind = kFlutterPointerSignalKindNone,
+                          .scroll_delta_x = 0.0,
+                          .scroll_delta_y = 0.0,
+                          .device_kind = kFlutterPointerDeviceKindTouch,
+                          .buttons = 0,
+                          .pan_x = 0,
+                          .pan_y = 0,
+                          .scale = 0,
+                          .rotation = 0});
+}
+
+void Engine::SendPointerEvents() {
+  if (!m_pointer_events.empty() && m_flutter_engine) {
+    std::scoped_lock lock(m_pointer_mutex);
+    LibFlutterEngine->SendPointerEvent(
+        m_flutter_engine, m_pointer_events.data(), m_pointer_events.size());
+    m_pointer_events.clear();
+  }
+}
+
+FlutterEngineAOTData Engine::LoadAotData(
+    const std::string& aot_data_path) const {
+  if (!fml::IsFile(aot_data_path)) {
+    SPDLOG_DEBUG("({}) AOT file not present", m_index);
+    return nullptr;
+  }
+
+  spdlog::info("({}) Loading AOT: {}", m_index, aot_data_path.c_str());
+
+  FlutterEngineAOTDataSource source = {};
+  source.type = kFlutterEngineAOTDataSourceTypeElfPath;
+  source.elf_path = aot_data_path.c_str();
+
+  FlutterEngineAOTData data;
+  if (kSuccess != LibFlutterEngine->CreateAOTData(&source, &data)) {
+    spdlog::critical("({}) Failed to load AOT data from: {}", m_index,
+                     aot_data_path);
+    return nullptr;
+  }
+  return data;
+}
+
+bool Engine::ActivateSystemCursor(const int32_t device, const std::string& kind) const {
+  return m_egl_window->ActivateSystemCursor(device, kind);
+}
+
+#if ENABLE_PLUGIN_TEXT_INPUT
+MAYBE_UNUSED void Engine::SetTextInput(TextInput* text_input) {
+  m_text_input = text_input;
+}
+
+MAYBE_UNUSED TextInput* Engine::GetTextInput() const {
+  return m_text_input;
+}
+
+#endif
+
+#if ENABLE_PLUGIN_KEY_EVENT
+MAYBE_UNUSED void Engine::SetKeyEvent(KeyEvent* key_event) {
+  m_key_event = key_event;
+}
+MAYBE_UNUSED KeyEvent* Engine::GetKeyEvent() const {
+  return m_key_event;
+}
+#endif
---
+++ test/unit_test/texture-test/test_case_texture.cc
@@ -0,0 +1,234 @@
+#include "gtest/gtest.h"
+#include "utils.h"
+#include "engine.h"
+#include "view/flutter_view.h"
+#include "wayland/display.h"
+#include "textures/texture.h"
+
+static constexpr char kSourceRoot[] = SOURCE_ROOT_DIR;
+constexpr int64_t kTestTextureObjectId = 5150;
+const std::string kCallCreateCb = "Call Create Callback";
+const flutter::EncodableValue kFlutterTestKey = flutter::EncodableValue("Test Key");
+const flutter::EncodableValue kFlutterTestVal = flutter::EncodableValue("Test Val");
+bool callDisposeCallback = false;
+
+/**
+ * @brief Get FlutterView instance
+ */
+FlutterView* createFlutterViewInstance() {
+  // setup parameter
+  struct Configuration::Config config {};
+  config.view.bundle_path = "/home/root/";
+  config.view.wl_output_index = 1;
+  auto configs = Configuration::ParseConfig(config);
+
+  // create FlutterView instance using mock
+  auto wayland_display = std::make_shared<Display>(false, "", "", configs);
+  auto* view = new FlutterView(config, 0, wayland_display);
+  return view;
+}
+
+/**
+ * @brief Get Engine instance
+ */
+Engine* createEngineInstance() {
+  // setup parameter
+  FlutterView* view = createFlutterViewInstance();
+  std::vector<const char*> vm_args_c;
+
+  // create Engine instance using mock
+  Engine *engine = new Engine(view, 1, vm_args_c, kSourceRoot, 1);
+  return engine;
+}
+
+/**
+ * @brief Callback function to registered instead of Texture::Create
+ */
+flutter::EncodableValue create_callback(void* userdata,
+    const flutter::EncodableMap* args) {
+  // check object Texture-ID
+  auto* obj = reinterpret_cast<Texture*>(userdata);
+  int64_t texture_id = obj->GetId();
+  EXPECT_EQ(kTestTextureObjectId, texture_id);
+
+  // check args key & value
+  EXPECT_TRUE(args->at(kFlutterTestKey) == kFlutterTestVal);
+
+  return flutter::EncodableValue(kCallCreateCb);
+}
+
+/**
+ * @brief Callback function to registered instead of Texture::Dispose
+ */
+void dispose_callback(void* userdata, GLuint name) {
+  // check object Texture-ID
+  auto* obj = reinterpret_cast<Texture*>(userdata);
+  int64_t texture_id = obj->GetId();
+  EXPECT_EQ(kTestTextureObjectId, texture_id);
+
+  // check name parameter
+  EXPECT_TRUE(kTestTextureObjectId == name);
+
+  callDisposeCallback = true;
+}
+
+/****************************************************************
+Test Case Name.Test Name： HomescreenTextureGetFlutterOpenGLTexture_Lv1Normal001
+Use Case Name: Set OpenGL texture
+Test Summary: Check that the value set in the constructor can be retrieved
+***************************************************************/
+TEST(HomescreenTextureGetFlutterOpenGLTexture, Lv1Normal001) {
+  // setup parameter
+  Texture *texture = new Texture(kTestTextureObjectId, GL_TEXTURE_2D,
+      GL_RGBA8, nullptr, nullptr);
+  ASSERT_TRUE(texture != nullptr);
+
+  // call target function
+  FlutterOpenGLTexture texture_out;
+  texture->GetFlutterOpenGLTexture(&texture_out);
+
+  // confirm to create instance
+  EXPECT_EQ(GL_TEXTURE_2D, texture_out.target);
+  EXPECT_EQ(GL_RGBA8, texture_out.format);
+}
+
+/****************************************************************
+Test Case Name.Test Name： HomescreenTextureCreate_Lv1Normal001
+Use Case Name: Set OpenGL texture
+Test Summary: Dont set callback and check default encodable value
+***************************************************************/
+TEST(HomescreenTextureCreate, Lv1Normal001) {
+  // setup parameter
+  Texture *texture = new Texture(kTestTextureObjectId, GL_TEXTURE_2D,
+      GL_RGBA8, nullptr, nullptr);
+  ASSERT_TRUE(texture != nullptr);
+
+  // call target function
+  flutter::EncodableValue create = texture->Create(100, 200, nullptr);
+
+  // confirm to create instance
+  flutter::EncodableValue expect_val = flutter::EncodableValue(
+    flutter::EncodableMap{
+      {flutter::EncodableValue("result"),
+       flutter::EncodableValue(-1)},
+      {flutter::EncodableValue("error"),
+       flutter::EncodableValue("Create callback not set")}});
+  EXPECT_TRUE(expect_val == create);
+}
+
+/****************************************************************
+Test Case Name.Test Name： HomescreenTextureCreate_Lv1Normal002
+Use Case Name: Set OpenGL texture
+Test Summary: set create callback and check if the callback is called
+***************************************************************/
+TEST(HomescreenTextureCreate, Lv1Normal002) {
+  // setup parameter
+  Texture *texture = new Texture(kTestTextureObjectId, GL_TEXTURE_2D,
+      GL_RGBA8, create_callback, nullptr);
+  ASSERT_TRUE(texture != nullptr);
+
+  flutter::EncodableMap test_args = flutter::EncodableMap{
+        {kFlutterTestKey, kFlutterTestVal}};
+
+  // call target function
+  flutter::EncodableValue create = texture->Create(100, 200, &test_args);
+
+  // confirm to call create_callback function
+  flutter::EncodableValue expect_val = flutter::EncodableValue(kCallCreateCb);
+  EXPECT_TRUE(expect_val == create);
+}
+
+/****************************************************************
+Test Case Name.Test Name： HomescreenTextureDispose_Lv1Normal001
+Use Case Name: Set OpenGL texture
+Test Summary: set dispose callback and check if the callback is called
+***************************************************************/
+TEST(HomescreenTextureDispose, Lv1Normal001) {
+  // setup parameter
+  Texture *texture = new Texture(kTestTextureObjectId, GL_TEXTURE_2D,
+      GL_RGBA8, nullptr, dispose_callback);
+  ASSERT_TRUE(texture != nullptr);
+  callDisposeCallback = false;
+
+  flutter::EncodableMap test_args = flutter::EncodableMap{
+        {kFlutterTestKey, kFlutterTestVal}};
+
+  // call target function
+  texture->Dispose(kTestTextureObjectId);
+
+  // confirm to call create_callback function
+  EXPECT_TRUE(callDisposeCallback);
+}
+
+/****************************************************************
+Test Case Name.Test Name： HomescreenTextureDispose_Lv1Normal002
+Use Case Name: Set OpenGL texture
+Test Summary: Dont set dispose callback and check if the callback is not called
+***************************************************************/
+TEST(HomescreenTextureDispose, Lv1Normal002) {
+  // setup parameter
+  Texture *texture = new Texture(kTestTextureObjectId, GL_TEXTURE_2D,
+      GL_RGBA8, nullptr, nullptr);
+  ASSERT_TRUE(texture != nullptr);
+  callDisposeCallback = false;
+
+  flutter::EncodableMap test_args = flutter::EncodableMap{
+        {kFlutterTestKey, kFlutterTestVal}};
+
+  // call target function
+  texture->Dispose(kTestTextureObjectId);
+
+  // confirm to call create_callback function
+  EXPECT_FALSE(callDisposeCallback);
+}
+
+/****************************************************************
+Test Case Name.Test Name： HomescreenTextureEnable_Lv1Normal001
+Use Case Name: Set OpenGL texture
+Test Summary: Check the texture ID is set
+***************************************************************/
+TEST(HomescreenTextureEnable, Lv1Normal001) {
+  // setup parameter
+  Texture *texture = new Texture(kTestTextureObjectId, GL_TEXTURE_2D,
+      GL_RGBA8, nullptr, nullptr);
+  ASSERT_TRUE(texture != nullptr);
+
+  Engine* engine = createEngineInstance();
+  texture->SetEngine(engine);
+
+  // call target function
+  texture->Enable(kTestTextureObjectId);
+
+  // confirm to create instance
+  EXPECT_EQ(texture->m_enabled, true);
+
+  // confirm to add name
+  auto it = std::find(texture->m_name.begin(), texture->m_name.end(), kTestTextureObjectId);
+  EXPECT_TRUE(texture->m_name.end() != it);
+}
+
+/****************************************************************
+Test Case Name.Test Name： HomescreenTextureDisable_Lv1Normal001
+Use Case Name: Set OpenGL texture
+Test Summary: Check the texture ID is deleted
+***************************************************************/
+TEST(HomescreenTextureDisable, Lv1Normal001) {
+  // setup parameter
+  Texture *texture = new Texture(kTestTextureObjectId, GL_TEXTURE_2D,
+      GL_RGBA8, nullptr, nullptr);
+  ASSERT_TRUE(texture != nullptr);
+
+  Engine* engine = createEngineInstance();
+  texture->SetEngine(engine);
+  texture->Enable(kTestTextureObjectId);
+
+  // call target function
+  texture->Disable(kTestTextureObjectId);
+
+  // confirm to create instance and add name
+  EXPECT_EQ(texture->m_enabled, false);
+
+  auto it = std::find(texture->m_name.begin(), texture->m_name.end(),
+      kTestTextureObjectId);
+  EXPECT_TRUE(texture->m_name.end() == it);
+}
