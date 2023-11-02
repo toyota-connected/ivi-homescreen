60c50e330e4147649ecad9d25562f1157eac6339
Joel Winarske <joel.winarske@toyotaconnected.com>
2023-11-02 14:36:30 -0700

    Release build CI
    -fix warnings to enable release build CI
    
    Signed-off-by: Joel Winarske <joel.winarske@toyotaconnected.com>

--- shell/view/compositor_surface.cc
+++ shell/view/compositor_surface.cc
@@ -56,7 +56,7 @@ CompositorSurface::CompositorSurface(
 
   auto parent_surface = window->GetBaseSurface();
 
-  if (type == CompositorSurface::egl) {
+  if (m_type == CompositorSurface::egl) {
     m_wl.egl_display = eglGetPlatformDisplay(EGL_PLATFORM_WAYLAND_KHR,
                                              display->GetDisplay(), nullptr);
     m_wl.egl_window = wl_egl_window_create(m_wl.surface, width, height);
