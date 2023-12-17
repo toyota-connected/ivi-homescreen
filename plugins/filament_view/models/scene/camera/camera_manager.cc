8c2345b26f9b6d714dca9d593263027b8bd714b6
Joel Winarske <joel.winarske@toyotaconnected.com>
2023-12-17 12:11:07 -0800

    Camera Manipulator
    Signed-off-by: Joel Winarske <joel.winarske@toyotaconnected.com>

--- plugins/filament_view/models/scene/camera/camera_manager.cc
+++ plugins/filament_view/models/scene/camera/camera_manager.cc
@@ -9,28 +9,44 @@ CameraManager::CameraManager(CustomModelViewer* model_viewer)
       engine_(model_viewer->getEngine()),
       view_(model_viewer->getView()) {
   SPDLOG_TRACE("++CameraManager::CameraManager");
-  utils::EntityManager& em = utils::EntityManager::get();
-  cameraEntity_ = em.create();
-  camera_ = engine_->createCamera(cameraEntity_);
+  setDefaultCamera();
   SPDLOG_TRACE("--CameraManager::CameraManager");
 }
 
+void CameraManager::setDefaultCamera() {
+  SPDLOG_TRACE("++CameraManager::setDefaultCamera");
+  const auto size = view_->getViewport();
+  manipulator_ = CameraManipulator::Builder()
+                          .targetPosition(
+                              kDefaultObjectPosition.x,
+                              kDefaultObjectPosition.y,
+                              kDefaultObjectPosition.z
+                              )
+                          .viewport(static_cast<int>(size.width), static_cast<int>(size.height))
+                          .build(::filament::camutils::Mode::ORBIT);
+  cameraEntity_ = engine_->getEntityManager().create();
+  camera_ = engine_->createCamera(cameraEntity_);
+  camera_->setExposure(
+        kAperture,
+        kShutterSpeed,
+        kSensitivity
+    );
+  SPDLOG_TRACE("--CameraManager::setDefaultCamera");
+}
+
 void CameraManager::updateCamera(Camera* camera) {
-  SPDLOG_DEBUG("CameraManager::updateCamera");
+  SPDLOG_TRACE("++CameraManager::updateCamera");
+  SPDLOG_TRACE("--CameraManager::updateCamera");
 }
 
 void CameraManager::lookAtDefaultPosition() {
-  // SPDLOG_DEBUG("CameraManager::lookAtDefaultPosition");
-  view_->setCamera(camera_);
-  const auto size = view_->getViewport();
-  displaySize_ =
-      ImVec2(static_cast<float>(size.width), static_cast<float>(size.height));
-  displayFramebufferScale_.x = 1;
-  displayFramebufferScale_.y = 1;
-  camera_->setProjection(::filament::Camera::Projection::ORTHO, 0.0,
-                         double(size.width), double(size.height), 0.0, 0.0,
-                         1.0);
-  camera_->lookAt({0.0f, 0.0f, kCameraDist}, kCameraCenter, kCameraUp);
+  SPDLOG_TRACE("++CameraManager::lookAtDefaultPosition");
+  if (manipulator_ && camera_) {
+    filament::math::float3 eye, center, up;
+    manipulator_->getLookAt(&eye, &center, &up);
+    camera_->lookAt(eye, center, up);
+  }
+  SPDLOG_TRACE("--CameraManager::lookAtDefaultPosition");
 }
 
 void CameraManager::destroyCamera() {
@@ -38,6 +54,18 @@ void CameraManager::destroyCamera() {
   engine_->destroyCameraComponent(cameraEntity_);
   utils::EntityManager::get().destroy(cameraEntity_);
   SPDLOG_DEBUG("--CameraManager::destroyCamera");
+}
+
+void CameraManager::onPointerDown(int x, int y) {
+  manipulator_->grabBegin(x, y, false);
+}
+
+void CameraManager::onPointerMove(int x, int y) {
+  manipulator_->grabUpdate(x, y);
+}
+
+void CameraManager::onPointerUp(int x, int y) {
+  manipulator_->grabEnd();
 }
 
 }  // namespace plugin_filament_view
