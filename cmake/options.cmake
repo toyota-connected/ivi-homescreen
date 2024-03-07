fe6250670b54391949a0d3e0977108a4ec8e1c53
Joel Winarske <joel.winarske@toyotaconnected.com>
2024-03-07 12:58:31 -0800

    OSMesa IMPORTED_TARGET
    Signed-off-by: Joel Winarske <joel.winarske@toyotaconnected.com>

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -84,7 +84,7 @@ endif ()
 option(BUILD_BACKEND_HEADLESS "Build Headless Backend" OFF)
 if (BUILD_BACKEND_HEADLESS)
     find_package(PkgConfig)
-    pkg_check_modules(OSMESA osmesa glesv2 egl REQUIRED)
+    pkg_check_modules(OSMESA osmesa glesv2 egl IMPORTED_TARGET REQUIRED)
     add_compile_definitions(BUILD_BACKEND_HEADLESS)
 endif ()
 
