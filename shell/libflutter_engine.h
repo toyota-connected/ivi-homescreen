80278ec61ec76b31941166d6609795cf7b722457
Joel Winarske <joel.winarske@toyotaconnected.com>
2024-07-29 09:58:34 -0700

    implement vm message handler
    -implement vm message handler
    -roll embedder.h
    -add shutdown to libengine wrapper
    -remove vm_queue scheme
    
    Signed-off-by: Joel Winarske <joel.winarske@toyotaconnected.com>

--- shell/libflutter_engine.h
+++ shell/libflutter_engine.h
@@ -37,16 +37,13 @@ struct LibFlutterEngineExports {
       PlatformMessageCreateResponseHandle = nullptr;
   FlutterEnginePlatformMessageReleaseResponseHandleFnPtr
       PlatformMessageReleaseResponseHandle = nullptr;
-  FlutterEngineSendPlatformMessageResponseFnPtr SendPlatformMessageResponse =
-      nullptr;
+  FlutterEngineSendPlatformMessageResponseFnPtr SendPlatformMessageResponse = nullptr;
   FlutterEngineRegisterExternalTextureFnPtr RegisterExternalTexture = nullptr;
-  FlutterEngineUnregisterExternalTextureFnPtr UnregisterExternalTexture =
-      nullptr;
+  FlutterEngineUnregisterExternalTextureFnPtr UnregisterExternalTexture = nullptr;
   FlutterEngineMarkExternalTextureFrameAvailableFnPtr
       MarkExternalTextureFrameAvailable = nullptr;
   FlutterEngineUpdateSemanticsEnabledFnPtr UpdateSemanticsEnabled = nullptr;
-  FlutterEngineUpdateAccessibilityFeaturesFnPtr UpdateAccessibilityFeatures =
-      nullptr;
+  FlutterEngineUpdateAccessibilityFeaturesFnPtr UpdateAccessibilityFeatures = nullptr;
   FlutterEngineDispatchSemanticsActionFnPtr DispatchSemanticsAction = nullptr;
   FlutterEngineOnVsyncFnPtr OnVsync = nullptr;
   FlutterEngineReloadSystemFontsFnPtr ReloadSystemFonts = nullptr;
