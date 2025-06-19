toyota-connected/ivi-homescreen

commit 0e52d4d1a3b9e4bd59ca512fc875163d58760142

compiler logic
-roll waypp
-toolchain::toolchain target
-gnu and clang cmake files

Signed-off-by: Joel Winarske <joel.winarske@gmail.com>

shell/wayland/window.cc
@@ -104,7 +104,7 @@ WaylandWindow::WaylandWindow(size_t index,
       break;
     case WINDOW_BG:
       m_display->AglShellDoBackground(m_base_surface, 0);
-      if (m_activation_area.x >= 0 && m_activation_area.y >= 0)
+      if (m_activation_area.x && m_activation_area.y)
         m_display->AglShellDoSetupActivationArea(
             m_activation_area.x, m_activation_area.y, m_activation_area.width,
             m_activation_area.height, 0);

CMakeLists.txt
@@ -34,7 +34,7 @@ project(ivi-homescreen
         LANGUAGES CXX C
 )
 
-include(context)
+include(git)
 include(options)
 
 message(STATUS "Project ................ ${PROJECT_NAME}")
@@ -43,7 +43,6 @@ message(STATUS "Generator .............. ${CMAKE_GENERATOR}")
 message(STATUS "Build Type ............. ${CMAKE_BUILD_TYPE}")
 
 include(compiler)
-include(utils)
 
 configure_file(cmake/config_common.h.in ${PROJECT_BINARY_DIR}/config/common.h)
 

cmake/compiler.cmake
@@ -1,5 +1,5 @@
 #
-# Copyright 2023 Toyota Connected North America
+# Copyright 2023-2025 Toyota Connected North America
 # @copyright Copyright (c) 2022 Woven Alpha, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -44,7 +44,7 @@ endfunction(COMPILER_FLAGS_APPEND)
 
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
 set(CMAKE_CXX_STANDARD 17)
-# set(CMAKE_CXX_EXTENSIONS OFF)
+set(CMAKE_CXX_EXTENSIONS OFF)
 
 set(CMAKE_POSITION_INDEPENDENT_CODE ON)
 set(CMAKE_VISIBILITY_INLINES_HIDDEN ON)
@@ -88,9 +88,6 @@ COMPILER_FLAGS_APPEND(RELEASE " -fno-omit-frame-pointer" "-f(no-)?omit-frame-poi
 COMPILER_FLAGS_APPEND(RELEASE " -Wformat=2" "-Wformat(=[0-9]+)?")
 COMPILER_FLAGS_APPEND(RELEASE " -D_FORTIFY_SOURCE=2" "-D_FORTIFY_SOURCE(=[0-9]+)?")
 
-# pigeon requires it
-string(APPEND CMAKE_CXX_FLAGS " -frtti")
-
 string(APPEND CMAKE_EXE_LINKER_FLAGS " -Wl,--build-id=sha1")
 
 string(APPEND CMAKE_EXE_LINKER_FLAGS_RELEASE " -Wl,--no-undefined")
@@ -115,3 +112,35 @@ message(STATUS "CC ..................... ${CMAKE_C_COMPILER} ${CMAKE_C_COMPILER_
 message(STATUS "CXX .................... ${CMAKE_C_COMPILER} ${CMAKE_CXX_COMPILER_ARG1}")
 message(STATUS "CFLAGS ................. ${CMAKE_C_FLAGS}")
 message(STATUS "CXXFLAGS ............... ${CMAKE_CXX_FLAGS}")
+
+#
+# Check for IPO support
+#
+if (ENABLE_LTO)
+    cmake_policy(SET CMP0069 NEW)
+    include(CheckIPOSupported)
+    check_ipo_supported(
+            RESULT IPO_SUPPORT_RESULT
+            OUTPUT IPO_SUPPORT_OUTPUT
+            LANGUAGES C CXX
+    )
+    if (IPO_SUPPORT_RESULT)
+        message(STATUS "IPO .................... supported")
+    else ()
+        message(STATUS "IPO .................... not supported: ${IPO_SUPPORT_OUTPUT}")
+    endif ()
+endif ()
+
+#
+# Compiler specific settings
+#
+message(STATUS "CMAKE_CXX_COMPILER_ID .. ${CMAKE_CXX_COMPILER_ID}")
+message(STATUS "CMAKE_CXX_COMPILER_VERSION: ${CMAKE_CXX_COMPILER_VERSION}")
+
+if (CMAKE_CXX_COMPILER_ID MATCHES "Clang")
+    include(compiler_clang)
+endif ()
+
+if (CMAKE_CXX_COMPILER_ID MATCHES "GNU")
+    include(compiler_gnu)
+endif ()

cmake/compiler_clang.cmake
@@ -1,0 +1,92 @@
+include_guard()
+
+cmake_policy(SET CMP0075 NEW)
+
+if (NOT LLVM_CONFIG)
+    find_program(LLVM_CONFIG "llvm-config" DOC "Path to llvm-config" REQUIRED)
+endif ()
+
+execute_process(
+        COMMAND ${LLVM_CONFIG} --cmakedir
+        OUTPUT_VARIABLE LLVM_DIR
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+)
+message(STATUS "LLVM_DIR ............... ${LLVM_DIR}")
+
+if (NOT EXISTS ${LLVM_DIR})
+    message(STATUS "LLVM cmakedir is specified and missing: ${LLVM_DIR}")
+    execute_process(
+            COMMAND ${LLVM_CONFIG} --version
+            OUTPUT_VARIABLE LLVM_PACKAGE_VERSION
+            OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+    execute_process(
+            COMMAND ${LLVM_CONFIG} --includedir
+            OUTPUT_VARIABLE LLVM_INCLUDE_DIRS
+            OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+    execute_process(
+            COMMAND ${LLVM_CONFIG} --libdir
+            OUTPUT_VARIABLE LLVM_LIBRARY_DIRS
+            OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+
+    set(LLVM_DEFINITIONS)
+    set(LLVM_ENABLE_ASSERTIONS OFF)
+    set(LLVM_ENABLE_EH OFF)
+    execute_process(
+            COMMAND ${LLVM_CONFIG} --has-rtti
+            OUTPUT_VARIABLE LLVM_ENABLE_RTTI_RAW
+            OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+    set(LLVM_ENABLE_RTTI OFF)
+    message(STATUS "llvm-config --has-rtti : ${LLVM_ENABLE_RTTI_RAW}")
+    if (${LLVM_ENABLE_RTTI_RAW} STREQUAL "YES")
+        set(LLVM_ENABLE_RTTI ON)
+    endif ()
+    set(LLVM_ENABLE_THREADS OFF)
+    execute_process(
+            COMMAND ${LLVM_CONFIG} --bindir
+            OUTPUT_VARIABLE LLVM_TOOLS_BINARY_DIR
+            OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+else ()
+    list(APPEND CMAKE_MODULE_PATH "${LLVM_CMAKE_DIR}")
+    find_package(LLVM REQUIRED CONFIG)
+endif ()
+
+message(STATUS "Found LLVM ............. ${LLVM_PACKAGE_VERSION}")
+message(STATUS "LLVM_INCLUDE_DIRS: ..... ${LLVM_INCLUDE_DIRS}")
+message(STATUS "LLVM_LIBRARY_DIRS: ..... ${LLVM_LIBRARY_DIRS}")
+message(STATUS "LLVM_DEFINITIONS: ...... ${LLVM_DEFINITIONS}")
+message(STATUS "LLVM_ENABLE_ASSERTIONS:  ${LLVM_ENABLE_ASSERTIONS}")
+message(STATUS "LLVM_ENABLE_EH: ........ ${LLVM_ENABLE_EH}")
+message(STATUS "LLVM_ENABLE_RTTI: ...... ${LLVM_ENABLE_RTTI}")
+message(STATUS "LLVM_ENABLE_THREADS: ... ${LLVM_ENABLE_THREADS}")
+message(STATUS "LLVM_TOOLS_BINARY_DIR: . ${LLVM_TOOLS_BINARY_DIR}")
+message(STATUS "C++ include path ....... ${LLVM_INCLUDE_DIRS}/c++/v1/")
+
+add_library(toolchain INTERFACE)
+add_library(toolchain::toolchain ALIAS toolchain)
+
+target_compile_definitions(toolchain INTERFACE "${LLVM_DEFINITIONS}")
+
+if (CMAKE_CXX_COMPILER_VERSION VERSION_GREATER_EQUAL 16)
+    message(STATUS "stdlib ................. libc++")
+    set(STDLIB libc++)
+else ()
+    message(STATUS "stdlib ................. libstdc++")
+    set(STDLIB libstdc++)
+endif ()
+
+target_compile_options(toolchain INTERFACE -stdlib=${STDLIB} -isystem"${LLVM_INCLUDE_DIRS}/c++/v1/")
+
+if (ENABLE_STATIC_LINK)
+    target_link_options(toolchain INTERFACE -static-libstdc++ -static-libgcc -stdlib=${STDLIB} -lc -fuse-ld=lld -v)
+else ()
+    target_link_options(toolchain INTERFACE -stdlib=${STDLIB} -lc -fuse-ld=lld -v)
+endif ()
+
+if (ENABLE_LTO AND IPO_SUPPORT_RESULT)
+    set_property(TARGET toolchain PROPERTY INTERPROCEDURAL_OPTIMIZATION TRUE)
+endif ()

cmake/compiler_gnu.cmake
@@ -1,0 +1,14 @@
+include_guard()
+
+add_library(toolchain INTERFACE)
+add_library(toolchain::toolchain ALIAS toolchain)
+
+if (ENABLE_STATIC_LINK)
+    target_link_options(toolchain INTERFACE -static-libstdc++ -static-libgcc -lc -v)
+else ()
+    target_link_options(toolchain INTERFACE -lc -v)
+endif ()
+
+if (ENABLE_LTO AND IPO_SUPPORT_RESULT)
+    set_property(TARGET toolchain PROPERTY INTERPROCEDURAL_OPTIMIZATION TRUE)
+endif ()

cmake/context.cmake
@@ -1,96 +1,0 @@
-#
-# Branch
-#
-execute_process(
-        COMMAND git rev-parse --abbrev-ref HEAD
-        WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
-        OUTPUT_VARIABLE GIT_BRANCH
-        OUTPUT_STRIP_TRAILING_WHITESPACE
-)
-
-if (GIT_BRANCH)
-    message(STATUS "GIT Branch ............. ${GIT_BRANCH}")
-else ()
-    set(GIT_BRANCH "unknown")
-endif ()
-
-#
-# Commit Hash
-#
-execute_process(
-        COMMAND git rev-parse --short HEAD
-        WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
-        OUTPUT_VARIABLE GIT_COMMIT_HASH
-        OUTPUT_STRIP_TRAILING_WHITESPACE
-)
-
-if (GIT_COMMIT_HASH)
-    message(STATUS "GIT Hash ............... ${GIT_COMMIT_HASH}")
-else ()
-    set(GIT_COMMIT_HASH "unknown")
-endif ()
-
-#
-# libc++
-#
-if (CMAKE_CXX_COMPILER_ID MATCHES "Clang")
-    if (NOT LLVM_CONFIG)
-        set(LLVM_CONFIG llvm-config)
-    endif ()
-    execute_process(
-            COMMAND ${LLVM_CONFIG} --version
-            OUTPUT_VARIABLE LLVM_VERSION
-            OUTPUT_STRIP_TRAILING_WHITESPACE
-    )
-    if (LLVM_VERSION)
-        message(STATUS "LLVM Version ........... ${LLVM_VERSION}")
-    endif ()
-
-    if (NOT LLVM_ROOT)
-        execute_process(
-                COMMAND ${LLVM_CONFIG} --prefix
-                OUTPUT_VARIABLE LLVM_ROOT
-                OUTPUT_STRIP_TRAILING_WHITESPACE
-        )
-    endif ()
-
-    if (NOT LLVM_ROOT)
-        message(WARNING "LLVM_ROOT not detected, using system default")
-        set(LLVM_ROOT "/usr")
-    endif ()
-
-    message(STATUS "LLVM Root .............. ${LLVM_ROOT}")
-    message(STATUS "C++ header path ........ ${LLVM_ROOT}/include/c++/v1/")
-
-    add_compile_options($<$<COMPILE_LANGUAGE:CXX>:-stdlib=libc++> $<$<COMPILE_LANGUAGE:CXX>:-isystem${LLVM_ROOT}/include/c++/v1/>)
-    set(CONTEXT_COMPILE_OPTIONS $<$<COMPILE_LANGUAGE:CXX>:-stdlib=libc++> $<$<COMPILE_LANGUAGE:CXX>:-isystem${LLVM_ROOT}/include/c++/v1/>)
-
-    execute_process(
-            COMMAND ${LLVM_CONFIG} --cmakedir
-            OUTPUT_VARIABLE LLVM_CMAKE_DIR
-            OUTPUT_STRIP_TRAILING_WHITESPACE
-    )
-    if (LLVM_CMAKE_DIR)
-        message(STATUS "Clang CMake Path ....... ${LLVM_CMAKE_DIR}")
-        list(APPEND CMAKE_MODULE_PATH ${LLVM_CMAKE_DIR})
-    endif ()
-
-endif ()
-
-#
-# Toolchain IPO/LTO support
-#
-if (ENABLE_LTO)
-    cmake_policy(SET CMP0069 NEW)
-    include(CheckIPOSupported)
-    check_ipo_supported(
-            RESULT IPO_SUPPORT_RESULT
-            OUTPUT IPO_SUPPORT_OUTPUT
-            LANGUAGES C CXX
-    )
-endif ()
-if (IPO_SUPPORT_RESULT)
-    message(STATUS "IPO .................... supported")
-else ()
-    message(STATUS "IPO .................... not supported: ${IPO_SUPPORT_OUTPUT}")
-endif ()

cmake/git.cmake
@@ -1,0 +1,33 @@
+include_guard()
+
+#
+# Branch
+#
+execute_process(
+        COMMAND git rev-parse --abbrev-ref HEAD
+        WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+        OUTPUT_VARIABLE GIT_BRANCH
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+)
+
+if (GIT_BRANCH)
+    message(STATUS "GIT Branch ............. ${GIT_BRANCH}")
+else ()
+    set(GIT_BRANCH "unknown")
+endif ()
+
+#
+# Commit Hash
+#
+execute_process(
+        COMMAND git rev-parse --short HEAD
+        WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+        OUTPUT_VARIABLE GIT_COMMIT_HASH
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+)
+
+if (GIT_COMMIT_HASH)
+    message(STATUS "GIT Hash ............... ${GIT_COMMIT_HASH}")
+else ()
+    set(GIT_COMMIT_HASH "unknown")
+endif ()

cmake/options.cmake
@@ -124,7 +124,7 @@ endif ()
 #
 # Static linking
 #
-option(ENABLE_CLANG_STATIC_LINK "Link libc++, libc++abi, and gcc as static libs when building with clang" OFF)
+option(ENABLE_STATIC_LINK "Link stdlib with static libs" OFF)
 
 #
 # Docs

commit 30a8284671b944724f5034b6d99bbc43c42f58e9

Fix AGL shell activation area behavior
Commit 0e52d4d1 accidentally broke using an activation area
configuration with either x or y set to 0.  Rework the logic
to use the specified configuration area if any of the position
or size values are non-zero.  This restores expected behavior
in the downstream AGL demos.  As well, drop the fallback
default activation area configuration, as it seems better to
not configure it at all if it has not been explicitly
configured.

Signed-off-by: Scott Murray <scott.murray@konsulko.com>

shell/wayland/window.cc
@@ -99,13 +99,11 @@ WaylandWindow::WaylandWindow(const size_t index,
       break;
     case WINDOW_BG:
       m_display->AglShellDoBackground(m_base_surface, 0);
-      if (m_activation_area.x && m_activation_area.y)
+      if (m_activation_area.x || m_activation_area.y ||
+	  m_activation_area.width || m_activation_area.height)
         m_display->AglShellDoSetupActivationArea(
             m_activation_area.x, m_activation_area.y, m_activation_area.width,
             m_activation_area.height, 0);
-      else
-        m_display->AglShellDoSetupActivationArea(
-            0, 160, m_activation_area.width, m_activation_area.height, 0);
       break;
     case WINDOW_PANEL_TOP:
       m_display->AglShellDoPanel(m_base_surface, AGL_SHELL_EDGE_TOP, 0);